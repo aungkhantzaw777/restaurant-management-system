481ad6345ffe353e088719667851e010a639c820
Aung Khant Zaw <aungkhantzaw133@gmail.com>
2023-05-01 20:44:55 +0630

    feature/fe-v-2

---
+++ resources/js/Pages/Menu.jsx
@@ -0,0 +1,14 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Menu({ auth, laravelVersion, phpVersion }) {
+    return (
+        <>
+            <Head title="Menu Management system" />
+            <div>
+              hello menu
+            </div>
+
+            
+        </>
+    );
+}
