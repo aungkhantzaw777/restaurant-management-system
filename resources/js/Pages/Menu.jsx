6797be237436d9007114ec3b98e38c95fde90e29
Aung Khant Zaw <aungkhantzaw133@gmail.com>
2023-05-02 22:08:37 +0630

    update

--- resources/js/Pages/Menu.jsx
+++ resources/js/Pages/Menu.jsx
@@ -1,14 +1,22 @@
 import { Link, Head } from '@inertiajs/react';
+import { useState } from 'react';
 
 export default function Menu({ auth, laravelVersion, phpVersion }) {
+    const [count, setCount] = useState(0)
+
+    const addCount = () => {
+        setCount(count + 1)
+    }
     return (
         <>
             <Head title="Menu Management system" />
             <div>
               hello menu
+                <div>
+                    {count}
+                </div>
+              <button onClick={addCount}>count 1</button>
             </div>
-
-            
         </>
     );
 }
